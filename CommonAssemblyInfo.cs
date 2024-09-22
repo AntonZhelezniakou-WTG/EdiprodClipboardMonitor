91c9856dc902d0416dbd1fad25eae15631a05682
Anton Zhelezniakou <Anton.Zhelezniakou@wisetechglobal.com>
2024-09-22 21:48:31 +0200

    Initial repo files

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyCompany("WiseTech Global")]
+[assembly: AssemblyProduct("EdiProd Clipboard Monitor")]
+[assembly: AssemblyCopyright("Copyright WiseTech Global 2001-2024")]
+[assembly: AssemblyTrademark("CargoWise One")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
+[assembly: NeutralResourcesLanguage("en-US")]
